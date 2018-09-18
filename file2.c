76ef2b961d62d2a9a38557e38919aa00ec6cff5c
sandhiya812 <43338242+sandhiya812@users.noreply.github.com>
2018-09-18 15:43:13 +0530

    Create file2.c

---
+++ file2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("PRINT\n");
+scanf("%d",&a);
+if((a%2==0)&&(a>=0))
+{
+printf("Even");
+}
+else if 
+{
+printf("Invalid");
+}
+else
+{
+printf("Odd");
+}
+getch();
+}
