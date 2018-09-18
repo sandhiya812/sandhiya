2b586f6b1c771aa19f9d0340dcebf36ce229d26c
sandhiya812 <43338242+sandhiya812@users.noreply.github.com>
2018-09-18 15:01:51 +0530

    Create file1.c

---
+++ file1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a:
+printf("\n");
+scanf("%d",&a);
+if(a==0)
+{
+printf("zero");
+}
+else if(a>0)
+{
+printf("positive");
+}
+else if(a<0)
+{
+printf("negative");
+}
+getch();
+}
